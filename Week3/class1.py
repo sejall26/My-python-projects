5205940d08ff4e03d1a637b677ea87438c1435c6
sejall26 <119845190+sejall26@users.noreply.github.com>
2023-04-06 03:32:45 +0530

    Create class1.py

---
+++ Week3/class1.py
@@ -0,0 +1,13 @@
+#Defining a class
+class Car:
+    bname = ""
+    model = ""
+
+#Creating object of class
+car1 = Car()
+
+#Accessing attributes and assigning them values
+car1.model = "Swift_178"
+car1.bname = "Maruti"
+
+print(f"Brand Name: {car1.bname}, Model: {car1.model} ")
